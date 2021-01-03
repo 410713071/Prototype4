9c63906980380f7b830917a07f18fd962edf8989
410713071 <410713071@gms.ndhu.edu.tw>
2021-01-04 02:52:58 +0800

    在平台中的某位置生成敵人

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;//建立enemy prefab變數
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);//將敵人建立在指定的XYZ上
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
