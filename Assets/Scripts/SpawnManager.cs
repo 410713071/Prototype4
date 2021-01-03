4a364e1655913b19b06aa9e15088eca233ac9093
410713071 <410713071@gms.ndhu.edu.tw>
2021-01-04 02:54:36 +0800

    在平台中的隨機位置生成敵人

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,13 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);//將敵人建立在指定的XYZ上
+        float spawnPosX = Random.Range(-9, 9);
+        float spawnPosZ = Random.Range(-9, 9);//經過測量發現平台大小大約落在9x9的範圍中
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);//在此範圍產生隨機一個地方並給予其一個變數儲存位置
+
+        Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+        
     }
 
     // Update is called once per frame
