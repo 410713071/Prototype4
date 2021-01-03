4e1019c2f8c85fc8effc08f46e37ebf002e2a099
410713071 <410713071@gms.ndhu.edu.tw>
2021-01-04 02:55:30 +0800

    試著讓程式碼變得更專業、好看

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;//建立enemy prefab變數
-
+    private float spawnRange = 9;//將void Start裡重複的9改成不被更改的變數
 
     // Start is called before the first frame update
     void Start()
     {
-        float spawnPosX = Random.Range(-9, 9);
-        float spawnPosZ = Random.Range(-9, 9);//經過測量發現平台大小大約落在9x9的範圍中
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);//將spawnRange代入原本寫9的地方
 
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);//在此範圍產生隨機一個地方並給予其一個變數儲存位置
 
